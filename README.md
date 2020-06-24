2f916ea1b23d7d11bd30decba66d1b5c0d30ae80
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2020-06-24 21:42:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+[![Build Status](https://travis-ci.org/mitsuruog/show-css-outline-style.svg?branch=master)](https://travis-ci.org/mitsuruog/show-css-outline-style) [![codecov](https://codecov.io/gh/mitsuruog/show-css-outline-style/branch/master/graph/badge.svg)](https://codecov.io/gh/mitsuruog/show-css-outline-style)
+
+# show-css-outline-style
+
+Chrome extensions that show the outline of all the elements that exist on the page.
+
+![demo](./assets/images/demo.gif)
+
+## Install
+
+<!-- - [Chrome web store](https://chrome.google.com/webstore/detail/nkbceicafhlobddjmlflmhbloffdekcd) -->
+
+## Features
+
+- Show the outline of all the elements that exist on the page.
+
+## License
+
+[MIT License](http://ja.wikipedia.org/wiki/MIT_License)
+(c) [mitsuruog](https://github.com/mitsuruog)
---
+++ src/background.ts
@@ -0,0 +1,27 @@
+const ID_NAME = "show-css-outline-style";
+const CSS_STYLE = "* { outline: 1px solid #F44336; }";
+
+chrome.browserAction.onClicked.addListener(({ url = "" }) => {
+  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    return;
+  }
+  chrome.tabs.executeScript({
+    code: `
+    ;(function() {
+      const head = document.querySelector("head");
+      const style = document.querySelector("style#${ID_NAME}");
+      if (head !== null) {
+        if (style === null) {
+          const newStyle = document.createElement("style");
+          newStyle.type = "text/css";
+          newStyle.appendChild(document.createTextNode("${CSS_STYLE}"));
+          newStyle.id = "${ID_NAME}";
+          head.appendChild(newStyle);
+        } else {
+          head.removeChild(style);
+        }
+      }
+    })();
+    `,
+  });
+});
